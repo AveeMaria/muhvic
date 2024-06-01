e8cc6703faaa2fd033e56583ba124fea64edb0da
AveeMaria <gasper.muhvic@protonmail.com>
2024-06-02 00:11:23 +0200

    making stuff prettier

--- muhvic/JS/script.js
+++ muhvic/JS/script.js
@@ -1,7 +1,10 @@
 console.log("zkj to beres?");
 
-setInterval(DisplayServerStats, 1000);
+let statRefreshTime = 1000;
 
+setInterval(DisplayServerStats, statRefreshTime);
+
+//pokaze ce streznik gori
 function DisplayServerStats() {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', '../PHP/sysinfo.php', true);
@@ -18,4 +21,19 @@ function DisplayServerStats() {
         console.error('Request failed');
     };
     xhr.send();
+}
+
+
+//displaya fake statse med body-ondload da se ne opaz delaya od commanda
+function DisplayFakeServerStats() {
+    //izjemno smesne stevilke, vem
+    const systemInfo = `
+        System load: 0.69 Temperature: 69.0 C<br>
+        Usage of /: 69.69% of 420.69GB Processes: 420<br>
+        Memory usage: 69% Users logged in: 420
+    `;
+
+    const systemInfoDiv = document.getElementById('systeminfo');
+
+    systemInfoDiv.innerHTML = systemInfo;
 }
