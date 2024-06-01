7883d7478cc03e3613a827c985f1d08081b8c0ee
AveeMaria <gasper.muhvic@protonmail.com>
2024-06-01 23:19:41 +0200

    bad file link

--- muhvic/JS/script.js
+++ muhvic/JS/script.js
@@ -1,24 +1,8 @@
 /*console.log("zkj to beres?");
 
-const {
-    exec
-}
-
-    = require('child_process');
-
-exec('landscape-sysinfo', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-
-    let systemInfoElement = document.getElementById('systeminfo');
-    systemInfoElement.textContent = stdout;
-});*/
-
 function DisplayServerStats() {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'PHP/sysinfo.php', true);
+    xhr.open('GET', '../PHP/sysinfo.php', true);
     xhr.onload = function () {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
