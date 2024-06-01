4e53ca3cd3486bf34f8acdd6b4f9b8b462908348
AveeMaria <gasper.muhvic@protonmail.com>
2024-06-01 23:18:19 +0200

    added displayserverstats function

--- muhvic/JS/script.js
+++ muhvic/JS/script.js
@@ -15,3 +15,21 @@ exec('landscape-sysinfo', (error, stdout, stderr) => {
     let systemInfoElement = document.getElementById('systeminfo');
     systemInfoElement.textContent = stdout;
 });*/
+
+function DisplayServerStats() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'PHP/sysinfo.php', true);
+    xhr.onload = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                document.getElementById('systeminfo').innerHTML = xhr.responseText;
+            } else {
+                console.error('Error:', xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function () {
+        console.error('Request failed');
+    };
+    xhr.send();
+}
